62cc7b229c121dd242fb140145fc193a7fc8c4cf
Hera-heeto <emil.soderlund@elev.ga.ntig.se>
2020-12-04 10:49:00 +0100

    commit

---
+++ src/AoC2020.java
@@ -0,0 +1,10 @@
+package src;
+
+import javax.swing.*;
+
+public class AoC2020 {
+    public static void main(String[] args) {
+        int a = 54;
+        JOptionPane.showInputDialog("what is a dog?");
+    }
+}
