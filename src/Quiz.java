db2a488975f9f4f4efdd7824d6ed813da4d0b76b
Hera-heeto <emil.soderlund@elev.ga.ntig.se>
2020-12-15 11:04:01 +0100

    Quiz is finished

---
+++ src/Quiz.java
@@ -0,0 +1,25 @@
+package src;
+
+import javax.swing.*;
+
+public class Quiz {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Answer my questions three and I shall set you free.");
+        String Q1 = JOptionPane.showInputDialog(null, "True or False: O'learys has consistently good meals");
+        if (Q1.equals("False")){
+            JOptionPane.showMessageDialog(null, "Correct answer");
+        } else {
+            JOptionPane.showMessageDialog(null, "YOU UTTER FOOL!!");
+        }
+        String Q2 = JOptionPane.showInputDialog(null, "Is 23 a prime number?");
+        if (Q2.equals("yes")){
+            JOptionPane.showMessageDialog(null, "Why yes of course");
+        } else {
+            JOptionPane.showMessageDialog(null, "YOU KNOW NOT THE BEAUTY OF 23?!");
+        }
+        String Q3 = JOptionPane.showInputDialog(null, "Can you see all of me, capture you or set you free. I am all, I am all, of me. Who am I?");
+        if (Q3.equals("Shadow the hedgehog")) {
+            JOptionPane.showMessageDialog(null, "Congratulations, you have passed the test. Now you get to squish dog.");
+        }
+    }
+}
