5761826b977a9b6de4e9f191760165e6095a403a
Hera-heeto <60388888+Emisod23@users.noreply.github.com>
2021-01-15 10:36:23 +0100

    börjat med hangman och börjat med att välja slump ord

---
+++ src/Hangman.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Hangman {
+    private static String pickrandomword;{
+
+    }
+
+    public static void main(String[] args) {
+        String word = pickrandomword;
+    }
+}
