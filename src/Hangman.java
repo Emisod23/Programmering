ede1ea1a200f518ab579acf8c4a9bd33009a15e9
Hera-heeto <emil.soderlund@elev.ga.ntig.se>
2021-01-19 10:44:52 +0100

    Fixade den ofungerade "random word generator"

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,13 +3,13 @@ package src;
 import javax.swing.*;
 
 public class Hangman {
-    private static String pickrandomword (){
+    private static String pickrandomname (){
         String[] names = {"Angelina", "Skadi","Silverash", "Ceobe", "Manticore", "Schwarz", "Blaze", "Exusiai", "Hoshiguma", "Saria", "Nightingale", "Shining", "Eyjafjalla", "Mostima", "Magallan", "W", "Blue Poison", "Lappland", "Astesia", "Specter", "Hellagur", "Sora", "Amiya", "Zima", "Warfarin", "Pramanix", "Liskarm", "Grani", "Bibeak", "Absinthe", "Indra", "Siege", "Bagpipe", "Glaucus", "Asbestos", "Sideroca", "Ceylon", "Istina", "Vulcan", "Snowsant", "Broca", "Silence", "Cliffheart", "FEater", "Mayer", "Nearl", "Platinum", "Ptilopsis", "Chen", "Aak", "Rosa", "Phantom", "Suzuran", "Weedy" };
         return names[(int) (Math.random() * names.length)];
     }
 
     public static void main(String[] args) {
-        String word = pickrandomword();
+        String word = pickrandomname();
         System.out.println(word);
     }
 }
