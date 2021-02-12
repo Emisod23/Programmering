aae71c94ef21ebefaefec3ed6bbcddaf17ac1dfb
Hera-heeto <60388888+Emisod23@users.noreply.github.com>
2021-02-12 10:53:49 +0100

    satte till text arean och knapparna

--- src/NoTE19pad.java
+++ src/NoTE19pad.java
@@ -1,5 +1,10 @@
 package src;
 
-public class NoTE19pad {
+import javax.swing.*;
 
+public class NoTE19pad {
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 }
