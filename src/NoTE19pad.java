fdda83f5ee2ec8be7eafbada3ad43ba32ac1ec1a
Hera-heeto <60388888+Emisod23@users.noreply.github.com>
2021-02-12 11:07:58 +0100

    Gedde knapparna namn

--- src/NoTE19pad.java
+++ src/NoTE19pad.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class NoTE19pad {
     private JTextArea textArea1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton nyFilButton;
+    private JButton sparaFilButton;
+    private JButton importeraFilButton;
 }
