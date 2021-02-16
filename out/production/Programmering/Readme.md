40d2c2332edb3015f537e12902490a58de483f3d
Hera-Heeto <Emil.soderlund@elev.ga.ntig.se>
2021-02-16 11:14:45 +0100

    pane go brrr

--- out/production/Programmering/Readme.md
+++ out/production/Programmering/Readme.md
@@ -16,3 +16,13 @@ utan att ha programmet glömma bort det originella
 ordet. Nästa lektion så ska jag väl försöka få
 en ett sätt att byta ut understräcken med vad som 
 spelaren matar in (vet inte riktigt dock).
+
+2021-01-15
+
+Jag har fått hjälp med att komma igång efter att ha missat
+en lektion och har nu lyckats med att få programmet 
+att fråga vad du vill gissa och sen hitta om din gissning
+är corekt. Sen så försökte jag börja med att byta ut
+i positionerna där svaret var men han inte tyvär.
+Nästa gång så kommer jag nog att behöva lite mer hjälp
+så att jag kan hinna med.
--- src/NoTE19pad.java
+++ src/NoTE19pad.java
@@ -1,10 +1,34 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NoTE19pad {
+    private JMenuBar menuBar;
+
+    public NoTE19pad() {
+        Clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea1.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+    JFrame frame = new JFrame("GUIManagerExample");
+    NoTE19pad gui = new NoTE19pad();
+    frame.setContentPane(gui.textArea1);
+    frame.setJMenuBar(gui.menuBar);
+    frame.setContentPane(gui.Clear);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+}
     private JTextArea textArea1;
-    private JButton nyFilButton;
-    private JButton sparaFilButton;
-    private JButton importeraFilButton;
+    private JButton Copy;
+    private JButton Clear;
+    private JButton button;
 }
